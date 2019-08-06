efc84dba4da63a029a05d3b94f19450f2e517ed6
Sammy Lee <sammy@Sammys-MacBook-Pro.local>
2019-08-06 14:43:47 -0700

    removed unnecessary code and files

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -2,8 +2,6 @@ class VendorsController < ApplicationController
   before_action :set_vendor, only: [:show, :edit, :update, :destroy]
   before_action :set_school, only: [:index]
 
-  # GET /vendors
-  # GET /vendors.json
   def index
     @vendors = Vendor.all
     @school = School.find_by(id: params[:school_id]) if params[:school_id]
@@ -11,62 +9,36 @@ class VendorsController < ApplicationController
     @vendors = @vendors.search(params[:keywords]) if params[:keywords].present?
   end
 
-  # GET /vendors/1
-  # GET /vendors/1.json
   def show
   end
 
-  # GET /vendors/new
-  def new
-    @vendor = Vendor.new
-  end
-
-  # GET /vendors/1/edit
   def edit
   end
 
-  # POST /vendors
-  # POST /vendors.json
   def create
     @vendor = Vendor.new(vendor_params)
 
-    respond_to do |format|
-      if @vendor.save
-        format.html { redirect_to @vendor, notice: 'Vendor was successfully created.' }
-        format.json { render :show, status: :created, location: @vendor }
-      else
-        format.html { render :new }
-        format.json { render json: @vendor.errors, status: :unprocessable_entity }
-      end
+    if @vendor.save
+      redirect_to @vendor, notice: 'Vendor was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /vendors/1
-  # PATCH/PUT /vendors/1.json
   def update
-    respond_to do |format|
-      if @vendor.update(vendor_params)
-        format.html { redirect_to @vendor, notice: 'Vendor was successfully updated.' }
-        format.json { render :show, status: :ok, location: @vendor }
-      else
-        format.html { render :edit }
-        format.json { render json: @vendor.errors, status: :unprocessable_entity }
-      end
+    if @vendor.update(vendor_params)
+      redirect_to @vendor, notice: 'Vendor was successfully updated.'
+    else
+      render :edit 
     end
   end
 
-  # DELETE /vendors/1
-  # DELETE /vendors/1.json
   def destroy
     @vendor.destroy
-    respond_to do |format|
-      format.html { redirect_to vendors_url, notice: 'Vendor was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to vendors_url, notice: 'Vendor was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_vendor
       @vendor = Vendor.find(params[:id])
     end
@@ -75,7 +47,6 @@ class VendorsController < ApplicationController
       @school = School.find(params[:school_id]) if params[:school_id]
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def vendor_params
       params.require(:vendor).permit(:name, :address, :phone, :email, :website, :description, :content)
     end
